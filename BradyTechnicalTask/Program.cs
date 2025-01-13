362cad495a14fc25258fa6e8865d0eee713fcadf
mancer23 <sanketbankhele@gmail.com>
2025-01-13 11:47:35 +0530

    code changes

--- BradyTechnicalTask/Program.cs
+++ BradyTechnicalTask/Program.cs
@@ -15,25 +15,11 @@ namespace BradyTechnicalTask
             {
                 FileSystemWatcher watcher = new FileSystemWatcher();
                 watcher.Path = inputFolderPath;
-
-                // Filter to watch only .xml files
-                watcher.Filter = "*.xml";
-
-                // Subscribe to the Created event (when a new file is added)
+                watcher.Filter = ReferenceData.Reference.XmlExtension;
                 watcher.Created += OnNewXmlFileAdded;
 
                 // Start watching
                 watcher.EnableRaisingEvents = true;
-
-                //var files = Directory.GetFiles(inputFolderPath, "*.xml");
-                //if (files.Length > 0)
-                //{
-                //    foreach (var file in files)
-                //    {
-                //        var xmlGeneration = new XmlGeneration();
-                //        xmlGeneration.GenerateXml(file, inputFolderPath);
-                //    }
-                //}
                 Console.ReadLine();
             }
             else
@@ -44,7 +30,6 @@ namespace BradyTechnicalTask
         }
         private static void OnNewXmlFileAdded(object sender, FileSystemEventArgs e)
         {
-            Console.WriteLine($"New XML file added: {e.Name}");
             var xmlGeneration = new XmlGeneration();
             xmlGeneration.GenerateXml(e.Name, ConfigurationManager.AppSettings["InputFolder"]);
         }
