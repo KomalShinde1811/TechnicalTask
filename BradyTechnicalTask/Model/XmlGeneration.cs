fb0fa677d544d9574c135efa3da664b540c2c5ee
mancer23 <sanketbankhele@gmail.com>
2025-01-13 12:54:22 +0530

    change

--- BradyTechnicalTask/Model/XmlGeneration.cs
+++ BradyTechnicalTask/Model/XmlGeneration.cs
@@ -13,7 +13,7 @@ namespace BradyTechnicalTask.Model
     {
         public void GenerateXml(string filename,string inputFolderPath)
         {
-            var data = MapXmlToGenerationReport(inputFolderPath+ Reference.BackSlash + filename);
+            var data = MapXmlToGenerationReport(filename);
             var outputData = new GenerationOutput()
             {
                 Totals = TotalGenerationsCalculation(data.Generators.ToList()),
@@ -146,7 +146,7 @@ namespace BradyTechnicalTask.Model
         private void GenerateOutPutFile(GenerationOutput generationOutput, string filename, string inputFolderPath)
         {
             string outpuytFilename = filename.Replace(inputFolderPath, string.Empty);
-            var filePath = ConfigurationManager.AppSettings["OutputFolder"] + Reference.BackSlash + outpuytFilename.Replace(Reference.XmlExtension, string.Empty) + "-Result.xml";
+            var filePath = ConfigurationManager.AppSettings["OutputFolder"] + outpuytFilename.Replace(Reference.XmlExtension, string.Empty) + "-Result.xml";
             var serializer = new XmlSerializer(typeof(GenerationOutput));
             using (var writer = new StreamWriter(filePath))
             {
--- BradyTechnicalTask/Program.cs
+++ BradyTechnicalTask/Program.cs
@@ -10,26 +10,51 @@ namespace BradyTechnicalTask
     {
         static void Main(string[] args)
         {
-            var inputFolderPath = ConfigurationManager.AppSettings["InputFolder"];
-            if (Directory.Exists(inputFolderPath))
-            {
-                FileSystemWatcher watcher = new FileSystemWatcher();
-                watcher.Path = inputFolderPath;
-                watcher.Filter = ReferenceData.Reference.XmlExtension;
-                watcher.Created += OnNewXmlFileAdded;
 
-                // Start watching
-                watcher.EnableRaisingEvents = true;
-                Console.ReadLine();
+            var path = ConfigurationManager.AppSettings["InputFolder"];
+            if (Directory.Exists(path))
+            {
+                Console.WriteLine($"Input folder exists: {path}");
+
+                var files = Directory.GetFiles(path, "*.xml");
+                if (files.Length > 0)
+                {
+                    Console.WriteLine("XML files found:");
+                    foreach (var file in files)
+                    {
+                        var xmlGeneration = new XmlGeneration();
+                        xmlGeneration.GenerateXml(file, path);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No XML files found in the folder.");
+                }
             }
             else
             {
-                Console.WriteLine($"Input folder does not exist: {inputFolderPath}");
+                Console.WriteLine($"Input folder does not exist: {path}");
             }
+            //var inputFolderPath = ConfigurationManager.AppSettings["InputFolder"];
+            //Console.WriteLine($"Input folder exist: {inputFolderPath}");
+            //if (Directory.Exists(inputFolderPath))
+            //{
+            //    FileSystemWatcher watcher = new FileSystemWatcher();
+            //    watcher.Path = inputFolderPath;
+            //    watcher.Filter =".xml";
+            //    watcher.Created += OnNewXmlFileAdded;
+            //    watcher.EnableRaisingEvents = true;
+            //    Console.ReadLine();
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"Input folder does not exist: {inputFolderPath}");
+            //}
 
         }
         private static void OnNewXmlFileAdded(object sender, FileSystemEventArgs e)
         {
+            Console.WriteLine($"File added: {e.Name}");
             var xmlGeneration = new XmlGeneration();
             xmlGeneration.GenerateXml(e.Name, ConfigurationManager.AppSettings["InputFolder"]);
         }
